42277a3a487869cd660748c58c21158bab0458fd
Dan Brewster <dnbrwstr@gmail.com>
2012-03-31 17:24:38 -0400

    ok

--- public/main.coffee
+++ public/main.coffee
@@ -1,7 +1,15 @@
+$(window).load ->
+	$('img').css height : $(window).height()
+	leftImg = $('#wrapper img:last')
+	rightPoint = leftImg.position().left + leftImg.outerWidth(true)
+	$('body').css height : rightPoint + 'px'
+	$('#wrapper').css width : rightPoint + 'px'
 
-$(document).ready ->
 
-
+$(window).scroll ->
+	top = $(window).scrollTop()
+	if (top - $(window).height() < $('#wrapper').width() - ($(window).width() - 1200) / 2)
+		$('#wrapper').css left : -$(window).scrollTop()
  
 
   
--- public/main.js
+++ public/main.js
@@ -1,5 +1,28 @@
 (function() {
 
-  $(document).ready(function() {});
+  $(window).load(function() {
+    var leftImg, rightPoint;
+    $('img').css({
+      height: $(window).height()
+    });
+    leftImg = $('#wrapper img:last');
+    rightPoint = leftImg.position().left + leftImg.outerWidth(true);
+    $('body').css({
+      height: rightPoint + 'px'
+    });
+    return $('#wrapper').css({
+      width: rightPoint + 'px'
+    });
+  });
+
+  $(window).scroll(function() {
+    var top;
+    top = $(window).scrollTop();
+    if (top - $(window).height() < $('#wrapper').width() - ($(window).width() - 1200) / 2) {
+      return $('#wrapper').css({
+        left: -$(window).scrollTop()
+      });
+    }
+  });
 
 }).call(this);
